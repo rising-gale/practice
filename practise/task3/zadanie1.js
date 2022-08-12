54caeff920f3ead167264a4931c6a8f5bd681858
Maks Rekalo <55653634+rising-gale@users.noreply.github.com>
2022-08-12 17:49:36 +0300

    Delete practise directory

--- practise/task3/zadanie1.js
+++
@@ -1,9 +0,0 @@
-function isEven(x) {
-    if ((x & 1) == 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// isEven(4);
--- practise/task4/zadanie5.js
+++
@@ -1,6 +0,0 @@
-function getNames(names)
-{
-    return names.replace(/(\w+), (\w+)/gi, '$2 $1');
-}
-
-// console.log(getNames("Lennon, John\nMcCartney, Paul\nHarrison, George\nStar, Ringo"));
